34644c373d4293d85621f8522fdb90366eb78d0f
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-29 18:23:06 +0530

    Update styles in button component

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -2,16 +2,29 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 const stylesByVariant = {
   primary: 'shadow-sm bg-primary text-white rounded-md',
   secondary: 'bg-white border border-primary text-primary rounded-1x',
 };
 
-export default function Button({ label, variant = 'primary', ...props }) {
+export default function Button({
+  label,
+  variant = 'primary',
+  fullWidth = false,
+  height = 'h-10',
+  ...props
+}) {
   return (
     <button
-      className={`capitalize py-2 px-4 text-base font-medium ${stylesByVariant[variant]}`}
+      className={cx(
+        `capitalize py-2 px-4 text-base font-medium hover:opacity-80 ${height} ${stylesByVariant[variant]}`,
+        {
+          'w-full': fullWidth,
+          'w-fit': !fullWidth,
+        }
+      )}
       {...props}
     >
       {label}
