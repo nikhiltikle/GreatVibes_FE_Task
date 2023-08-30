efb8bbc28fd821d73a2b340e664254135c611b91
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-30 14:24:55 +0530

    Update props error and label in radio button

--- components/RadioInput/index.jsx
+++ components/RadioInput/index.jsx
@@ -2,6 +2,9 @@ import { RadioGroup } from '@headlessui/react';
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import FormHelperText from '../FormHelperText';
+import Field from '../Field';
+import InputLabel from '../InputLabel';
 
 export default function RadioInput({
   label,
@@ -9,50 +12,59 @@ export default function RadioInput({
   onChange,
   options,
   direction = 'row',
+  error = '',
+  required = false,
+  ...props
 }) {
   const isRowDirection = direction === 'row';
 
   return (
-    <RadioGroup
-      value={value}
-      onChange={onChange}
-    >
-      <RadioGroup.Label className='font-medium text-sm'>
-        {label}
-      </RadioGroup.Label>
-
-      <div
-        className={cx('gap-4 flex', {
-          'py-2': isRowDirection,
-          'flex-col': !isRowDirection,
-        })}
+    <Field>
+      <RadioGroup
+        value={value}
+        onChange={onChange}
+        {...props}
       >
-        {options.map((option) => (
-          <RadioGroup.Option
-            key={option.id}
-            value={option.value}
-            className={
-              'relative flex cursor-pointer focus:outline-none gap-1 items-center text-sm font-normal text-placeholder'
-            }
-          >
-            {({ checked }) => (
-              <>
-                <div
-                  className={cx(
-                    'h-5 w-5 rounded-full flex justify-center items-center',
-                    {
-                      'border-[6px] border-primary': checked,
-                      'border-2 border-card': !checked,
-                    }
-                  )}
-                />
-                {option.label}
-              </>
-            )}
-          </RadioGroup.Option>
-        ))}
-      </div>
-    </RadioGroup>
+        <InputLabel
+          label={label}
+          required={required}
+        />
+
+        <div
+          className={cx('gap-4 flex', {
+            'py-2': isRowDirection,
+            'flex-col': !isRowDirection,
+          })}
+        >
+          {options.map((option) => (
+            <RadioGroup.Option
+              key={option.id}
+              value={option.value}
+              className={
+                'relative flex cursor-pointer focus:outline-none gap-1 items-center text-sm font-normal text-placeholder'
+              }
+            >
+              {({ checked }) => (
+                <>
+                  <div
+                    className={cx(
+                      'h-5 w-5 rounded-full flex justify-center items-center',
+                      {
+                        'border-[6px] border-primary': checked,
+                        'border-2 border-card': !checked,
+                      }
+                    )}
+                  />
+                  {option.label}
+                </>
+              )}
+            </RadioGroup.Option>
+          ))}
+        </div>
+      </RadioGroup>
+
+      {error && <FormHelperText error={error} />}
+    </Field>
   );
 }
 
@@ -60,6 +72,8 @@ RadioInput.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  required: PropTypes.bool,
   direction: PropTypes.oneOf(['row', 'column']),
   options: PropTypes.arrayOf(
     PropTypes.shape({
